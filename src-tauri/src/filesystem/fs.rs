33cf6000e207fd4415224278d903537a5169c836
impify <md.7m51@gmail.com>
2024-01-30 20:41:30 +0200

    Updated fs.rs from using standard fs to jwalk

--- src-tauri/src/filesystem/fs.rs
+++ src-tauri/src/filesystem/fs.rs
@@ -1,7 +1,6 @@
 use serde::Serialize;
-use std::fs;
-use std::path::PathBuf;
 use std::time::SystemTime;
+use jwalk::WalkDir;
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -17,7 +16,7 @@ pub struct DirMetadata {
 #[serde(rename_all = "camelCase")]
 pub struct DirEntry {
     name: String,
-    path: PathBuf,
+    path: String,
     size: u64,
     metadata: DirMetadata,
 }
@@ -26,30 +25,33 @@ pub struct DirEntry {
 pub fn read_directory(path: &str) -> Vec<DirEntry> {
     let mut result: Vec<DirEntry> = Vec::new();
 
-    if let Ok(entries) = fs::read_dir(path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                let file_name = entry.file_name();
-                let metadata = entry.metadata().unwrap();
-                let path = entry.path();
+    // Use jwalk to walk through the dir
+    for entry in WalkDir::new(path).into_iter().filter_map(Result::ok) {
+        let file_name = entry.file_name();
+        let metadata = entry.metadata().unwrap();
 
-                let dirmetadata = DirMetadata {
-                    is_dir: metadata.is_dir(),
-                    is_file: metadata.is_file(),
-                    modified: metadata.modified().unwrap(),
-                    accessed: metadata.accessed().unwrap(),
-                    created: metadata.created().unwrap(),
-                };
+        let dirmetadata = DirMetadata {
+            is_dir: metadata.is_dir(),
+            is_file: metadata.is_file(),
+            modified: metadata.modified().unwrap(),
+            accessed: metadata.accessed().unwrap(),
+            created: metadata.created().unwrap(),
+        };
 
-                result.push(DirEntry {
-                    name: file_name.to_str().unwrap().to_string(),
-                    path,
-                    size: metadata.len(),
-                    metadata: dirmetadata,
-                });
-            }
-        }
-    };
+        let path_str = entry.path().to_string_lossy().to_string();
+
+        result.push(DirEntry {
+            name: file_name.to_string_lossy().to_string(), // File name
+            path: path_str, // File path
+            size: metadata.len(), // Call len() method to get file size,
+            metadata: dirmetadata, // File metadata
+        });
+    }
+
+    // Remove the first object from the vector if it is not empty
+    if !result.is_empty() {
+        result.remove(0);
+    }
 
     result
 }
